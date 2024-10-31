3bd2317cfbb24c3522337d32772c7431fd7c02ae
hashem-wannous <hashem.wannous@unifi.solutions>
2024-10-31 20:30:51 +0300

    Refactorization.

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -7,7 +7,6 @@ public class MeshGenerator : MonoBehaviour
     [SerializeField] Vector2Int _size = Vector2Int.one * 256;
     [SerializeField] private Vector2 _offset = Vector2.one * 100;
     [SerializeField] private float _scale = 20;
-    [SerializeField] private float _offsetSpeed = 20;
 
     private Mesh _mesh;
     private int[] _triangles;
@@ -18,23 +17,17 @@ public class MeshGenerator : MonoBehaviour
         _mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = _mesh;
 
-        CreateShape();
+        InitializeTriangles();
+    }
+
+    private void Update()
+    {
+        UpdateVertices();
         UpdateMesh();
     }
 
-    private void CreateShape()
+    private void InitializeTriangles()
     {
-        _vertices = new Vector3[(_size.x + 1) * (_size.y + 1)];
-
-        for (int i = 0, x = 0; x < _size.x + 1; x++)
-        {
-            for (int y = 0; y < _size.y + 1; y++, i++)
-            {
-                float depth = Mathf.PerlinNoise((x - _size.x / 2f) * 0.3f, (y - _size.y / 2f) * 0.3f) * 2;
-                _vertices[i] = new Vector3(x - _size.x / 2f, depth, y - _size.y / 2f);
-            }
-        }
-
         _triangles = new int[_size.x * _size.y * 6];
         for (int vertexIndex = 0, triangleIndex = 0, x = 0; x < _size.x; x++, vertexIndex++)
         {
@@ -44,7 +37,23 @@ public class MeshGenerator : MonoBehaviour
                 _triangles[triangleIndex + 5] = vertexIndex + _size.x + 2;
                 _triangles[triangleIndex + 1] = _triangles[triangleIndex + 4] = vertexIndex + 1;
                 _triangles[triangleIndex + 2] = _triangles[triangleIndex + 3] = vertexIndex + _size.x + 1;
+            }
+        }
+    }
 
+    private void UpdateVertices()
+    {
+        _vertices = new Vector3[(_size.x + 1) * (_size.y + 1)];
+
+        for (int i = 0, x = 0; x < _size.x + 1; x++)
+        {
+            for (int y = 0; y < _size.y + 1; y++, i++)
+            {
+                float noiseX = (float)x / _size.x * _scale + _offset.x;
+                float noiseY = (float)y / _size.y * _scale + _offset.y;
+
+                float sample = Mathf.PerlinNoise(noiseX, noiseY);
+                _vertices[i] = new Vector3(x - _size.x / 2f, sample * _depth, y - _size.y / 2f);
             }
         }
     }
